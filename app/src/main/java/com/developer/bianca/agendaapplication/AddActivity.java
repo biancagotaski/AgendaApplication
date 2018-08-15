744bf5b7e93fcd1c265055524c87f41d8f9b4bb9
Bianca Gotaski <biancagotaski@outlook.com>
2018-08-15 15:31:41 -0300

    Com erro no recycler view, na chamada do app

--- app/src/main/java/com/developer/bianca/agendaapplication/AddActivity.java
+++ app/src/main/java/com/developer/bianca/agendaapplication/AddActivity.java
@@ -3,7 +3,6 @@ package com.developer.bianca.agendaapplication;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -22,8 +21,6 @@ public class AddActivity extends AppCompatActivity {
     EditText editTextTelephone;
     EditText editTextEmail;
     EditText editTextCity;
-
-    RecyclerView contactsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
